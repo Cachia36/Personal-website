2412d85ec2c6a56ff1f66ea524c3a0be6b342343
Cachia36 <kyle.cachia.f32070@mcast.edu.mt>
2025-06-23 22:57:31 +0200

    reverted title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Kyle | Freelance Web Developer',
-  description: 'Custom websites for entrepreneur, professionals, and businesses.',
+  title: 'v0 App',
+  description: 'Created with v0',
   generator: 'v0.dev',
 }
 
